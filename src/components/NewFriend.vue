f6b2ef50c0f769e731fa236db36ed87f4a635e78
Rodney Mupanduki <rodneym@afrosoft.co.zw>
2022-04-06 12:04:19 +0200

    first commit

---
+++ src/components/NewFriend.vue
@@ -0,0 +1,42 @@
+<template>
+    <form @submit.prevent="submitData">
+        <div>
+            <label for="name">Name</label>
+            <input type="text" v-model="enteredName">
+        </div>
+        <div>
+            <label for="name">Phone</label>
+            <input type="tel" v-model="enteredPhone">
+        </div>
+        <div>
+            <label for="name">E-mail</label>
+            <input type="email" v-model="enteredEmail">
+        </div>
+        <div>
+            <button>Add Contact</button>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    emits : ['add-contact'],
+    data(){
+        return {
+            enteredName : '',
+            enteredPhone: '',
+            enteredEmail : ''
+        }
+    },
+
+    methods: {
+        submitData(){
+            this.$emit('add-contact', {
+                name : this.enteredName,
+                phone: this.enteredPhone,
+                email: this.enteredEmail
+            })
+        }
+    }
+}
+</script>
